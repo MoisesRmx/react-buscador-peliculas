09b3c4690abf57d2467f514c995c38a8fe1d89bf
Moises Roberto Sanchez Perez <moisesrobertosanchezperez@gmail.com>
2024-04-04 02:41:52 -0600

    Segundo commit

---
+++ src/components/Movies.jsx
@@ -0,0 +1,36 @@
+export function ListOfMovies({ movies }) {
+  return (
+    <ul className="movies">
+      {
+        movies.map(({ id, title, year, poster }) => (
+          <li className="movie" key={id}>
+            <h3>{title}</h3>
+            <p>{year}</p>
+            <img src={poster} alt={title} />
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+export function NoMovies() {
+  return (
+    <p>No se encontraron películas</p>
+  )
+}
+
+export function Movies({ movies }) {
+  const hasMovies = movies?.length > 0
+
+  return (
+    hasMovies
+      ? (
+        <ListOfMovies movies={movies} />
+      )
+      : (
+        <NoMovies />
+      )
+
+  )
+}
